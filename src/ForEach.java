3ada36251dd04ed5d69e07fd11fb9991214255e1
AleksanderSm <aleksander.smardzewski@gmail.com>
2022-11-23 19:24:28 +0100

    Uzycie petli foreach

---
+++ src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cats = new String[]{"Bonifacy", "Garfield", "Klakier", "Pussy"};
+        for (String cat:cats) {
+            System.out.println(cat);
+        }
+
+    }
+}
