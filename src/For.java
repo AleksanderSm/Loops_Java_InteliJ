9783c21223ae5820a60eb45bfcc755243f1ab840
AleksanderSm <aleksander.smardzewski@gmail.com>
2022-11-23 18:58:20 +0100

    Uzycie pepli for do czytania znakow w tablicy

--- src/For.java
+++ src/For.java
@@ -1,7 +1,7 @@
 public class For {
     public static void main(String[] args) {
         String[] cats=new String[]{"Bonifacy","Garfield", "Klakier","Pussy" };
-        for (int i=0; i<4; i++) {
+        for (int i=0; i< cats.length; i++) {
             System.out.println(cats[i]);
         }
     }
